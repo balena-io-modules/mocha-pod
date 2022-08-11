03b81c18dd6c9264b388750e318e55eac574abe6
Felipe Lalanne <felipe@balena.io>
2022-08-11 17:55:42 -0400

    Improve the containerized environment detection

--- lib/attach/hooks.ts
+++ lib/attach/hooks.ts
@@ -12,11 +12,31 @@ export class MochaPodError extends Error {}
 export const mochaHooks = {
 	async beforeAll() {
 		// Confirm that we are running in a containerized environment
-		await fs.access('/.dockerenv').catch(() => {
-			throw new MochaPodError(
-				'Not running in a containerized environment. Exiting to avoid damage.',
-			);
-		});
+		await fs
+			// This works in OS X at least
+			.access('/.dockerenv')
+			.catch(() =>
+				// This should work most other places
+				fs.readFile('/proc/self/cgroup', 'utf8').then((contents) => {
+					const lines = contents.split(/\r?\n/);
+					if (
+						lines
+							.map((l) => l.split(':'))
+							.filter(([, , entry]) => entry && entry.startsWith('/docker'))
+							.length === 0
+					) {
+						// Throw a place holder error
+						throw new Error();
+					}
+				}),
+			)
+			// Catch any errors during the previous step. Do not run if there
+			// are any doubts about this being a containerized enviroment
+			.catch(() => {
+				throw new MochaPodError(
+					'It seems that you are in a containerized environment. Exiting to avoid damage.',
+				);
+			});
 
 		const backups = await globby(path.join(os.tmpdir(), 'mochapod-*.tar'));
 		if (backups.length > 0) {
